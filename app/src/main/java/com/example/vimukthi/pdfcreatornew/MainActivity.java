c1e9f05d661ee84ee246e85b08af27fea60d58a6
vimukthi96 <vimukthi072@gmail.com>
2018-11-11 21:51:10 +0530

    change filename to date and time also add a sub folder

--- app/src/main/java/com/example/vimukthi/pdfcreatornew/MainActivity.java
+++ app/src/main/java/com/example/vimukthi/pdfcreatornew/MainActivity.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.format.DateFormat;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,14 +16,17 @@ import com.itextpdf.text.DocumentException;
 import com.itextpdf.text.Paragraph;
 import com.itextpdf.text.pdf.PdfWriter;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.util.Date;
 
 
 public class MainActivity extends Activity {
 
     Button btnCreatePDF;
     EditText editText;
+  //  String fileName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,11 +42,22 @@ public class MainActivity extends Activity {
 
     }
     public void cretePDF(View view){
+        String path= Environment.getExternalStorageDirectory().getAbsolutePath() + "/PDF Creator";
+
+        File dir =new File(path);
+        if(!dir.exists()){
+            dir.mkdirs();
+        }
+        Date date = new Date();
+        CharSequence sequence  = DateFormat.format("MM-dd-yy hh-mm-ss", date.getTime());
+        Log.d("PDFCretor","PDF path" +path);
+        File file =new File(dir ,sequence.toString()+".pdf");
+
         editText=(EditText)findViewById(R.id.edtText);
         Document doc=new Document() ;
-        String outPath = Environment.getExternalStorageDirectory()+"/mypdf.pdf";
+
         try {
-            PdfWriter.getInstance(doc ,new FileOutputStream(outPath));
+            PdfWriter.getInstance(doc ,new FileOutputStream(file));
             doc.open();
             doc.add(new Paragraph(editText.getText().toString()));
             doc.close();
